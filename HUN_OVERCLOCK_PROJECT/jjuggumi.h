8a882c0608a494552ce554d0e0a773f5302eec1e
NaengEnamul <naengenamul@naver.com>
2023-11-24 10:10:25 +0900

    쭈꾸미 헤더파일 수정

--- HUN_OVERCLOCK_PROJECT/jjuggumi.h
+++ HUN_OVERCLOCK_PROJECT/jjuggumi.h
@@ -6,7 +6,24 @@
 
 #define PLAYER_MAX		10
 
-bool player[PLAYER_MAX];  // 기본값 true, 탈락하면 false
+
+typedef struct {
+	char name[100];
+	int intel_buf, str_buf, stamina_buf;
+}ITEM;
+
+typedef struct {
+	int id;
+	char name[100];
+
+	int intel, str, stamina;
+
+	bool is_alive;
+	bool hasitem;
+	ITEM item;
+} PLAYER;
+
+PLAYER player[PLAYER_MAX]; // 기본값 true, 탈락하면 false
 int n_player, n_alive, winner_player;
 int tick;  // 시계
 int stop_tick;
